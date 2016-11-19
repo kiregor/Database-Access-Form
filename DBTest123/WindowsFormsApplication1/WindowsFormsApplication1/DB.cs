402420e5d6d11cad089f09c7cae28d7e28cb009d
Throthgar <kwj101@student.anglia.ac.uk>
2016-11-19 18:27:38 +0000

    DataSavingTestProject
    Simeple testing project. Top button displays data, bottom adds a
    pre-generated data into the database.

---
+++ DBTest123/WindowsFormsApplication1/WindowsFormsApplication1/DB.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace WindowsFormsApplication1
+{
+    class DB
+    {
+        private static DB _instance;
+        private static string _DBConnectionString;
+        private static SqlConnection con;
+        private static SqlCommand comm;
+
+        public static string DBConnectionString
+        {
+            set
+            {
+                _DBConnectionString = value;
+            }
+        }
+
+        public static DB GetConInstance()
+        {
+            if (_instance == null)
+            {
+                _instance = new DB();
+            }
+
+            return _instance;
+        }
+
+        public void ConnectionOpen()
+        {
+            con = new SqlConnection(_DBConnectionString);
+            con.Open();
+        }
+
+        public void ConnectionCLose()
+        {
+            con.Close();
+        }
+
+        public void _SQLCommandSetup(string query)
+        {
+            comm = new SqlCommand();
+            comm.Connection = con;
+            comm.CommandText = query;
+        }
+
+        public DataSet DBconnection1(string q)
+        {
+            comm = new SqlCommand();
+            string x = q;
+            ConnectionOpen();
+            //_SQLCommandSetup(x);
+            comm.Connection = con;
+            comm.CommandText = q;
+            //comm.CommandText = "SELECT Content FROM tblContent WHERE TagID = 1";
+
+            DataSet dt = new DataSet();
+            SqlDataAdapter adapter = new SqlDataAdapter(comm);
+
+            adapter.Fill(dt);
+
+            ConnectionCLose();
+
+            return dt;
+        }
+    }
+}
---
+++ DBTest123/WindowsFormsApplication1/WindowsFormsApplication1/bdcontentaccess.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace WindowsFormsApplication1
+{
+    class bdcontentaccess
+    {
+        public static void mainn()
+        {
+            string s1 = "Select * From tbl123";
+            DataSet ds1 = DB.GetConInstance().DBconnection1(s1);
+            DataTable dtt = ds1.Tables[0];
+            dgv dgvv = new dgv();
+            dgvv.getGrid(dtt);
+            dgvv.ShowDialog();
+
+        }
+
+        public static void datainster()
+        {
+            using (SqlConnection connection = new SqlConnection(Properties.Settings.Default.MyConnectionString))
+            {
+                //string s1 = "insert into tbl123 (name, admin) Values (qwerty, true)";
+                SqlCommand cmd = new SqlCommand("insert into tbl123 (name, admin) Values (@name, @admin)");
+                cmd.CommandType = CommandType.Text;
+                cmd.Connection = connection;
+                cmd.Parameters.AddWithValue("@name", "kacper");
+                cmd.Parameters.AddWithValue("@admin", "true");
+                connection.Open();
+                cmd.ExecuteNonQuery();
+            }
+
+        }
+    }
+}
